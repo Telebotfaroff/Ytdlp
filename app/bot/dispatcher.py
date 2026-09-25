f70647f8376a1c961780303c543f02315c15ba42
Telebotfaroff <faroffwork@gmail.com>
2026-09-25 23:34:35 +0530

    Register Eporner command handler

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -4,12 +4,13 @@ from aiogram import Dispatcher
 
 from app.bot.callbacks import router as callback_router
 from app.bot.handlers import download
-from app.bot.handlers import start, url
+from app.bot.handlers import ep, start, url
 
 
 def create_dispatcher() -> Dispatcher:
     dp = Dispatcher()
     dp.include_router(start.router)
+    dp.include_router(ep.router)
     dp.include_router(url.router)
     dp.include_router(callback_router)
     dp.include_router(download.router)
