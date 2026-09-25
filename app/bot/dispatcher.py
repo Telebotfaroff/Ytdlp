39266f761493d46da5aceb8edd4bb16d016cbbd8
Telebotfaroff <faroffwork@gmail.com>
2026-09-25 22:04:40 +0530

    feat: register media callbacks

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -1,6 +1,8 @@
 """Create and configure the Telegram dispatcher."""
 
 from aiogram import Dispatcher
+
+from app.bot.callbacks import router as callback_router
 from app.bot.handlers import start, url
 
 
@@ -8,4 +10,5 @@ def create_dispatcher() -> Dispatcher:
     dp = Dispatcher()
     dp.include_router(start.router)
     dp.include_router(url.router)
+    dp.include_router(callback_router)
     return dp
