3d6df59ee8e38d598ddb3a036b7448dc13434c6b
Telebotfaroff <faroffwork@gmail.com>
2026-09-26 00:08:14 +0530

    Register paginated search callbacks

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -5,6 +5,7 @@ from aiogram import Dispatcher
 from app.bot.callbacks import router as callback_router
 from app.bot.handlers import download
 from app.bot.handlers import adult_sources, ep, start, url
+from app.bot.search.callbacks import router as search_router
 
 
 def create_dispatcher() -> Dispatcher:
@@ -13,6 +14,7 @@ def create_dispatcher() -> Dispatcher:
     dp.include_router(ep.router)
     dp.include_router(adult_sources.router)
     dp.include_router(url.router)
+    dp.include_router(search_router)
     dp.include_router(callback_router)
     dp.include_router(download.router)
     return dp
