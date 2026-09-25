3603ce65adbcfa9ac1ef0d59c34426c36431e199
Telebotfaroff <faroffwork@gmail.com>
2026-09-25 23:39:01 +0530

    Register separate adult source commands

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -4,13 +4,14 @@ from aiogram import Dispatcher
 
 from app.bot.callbacks import router as callback_router
 from app.bot.handlers import download
-from app.bot.handlers import ep, start, url
+from app.bot.handlers import adult_sources, ep, start, url
 
 
 def create_dispatcher() -> Dispatcher:
     dp = Dispatcher()
     dp.include_router(start.router)
     dp.include_router(ep.router)
+    dp.include_router(adult_sources.router)
     dp.include_router(url.router)
     dp.include_router(callback_router)
     dp.include_router(download.router)
