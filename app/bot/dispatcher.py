2b80cddba66c37f5dc60c5a8dcdec7c15d81f667
Telebotfaroff <faroffwork@gmail.com>
2026-09-25 22:06:49 +0530

    feat: register download handler

--- app/bot/dispatcher.py
+++ app/bot/dispatcher.py
@@ -3,6 +3,7 @@
 from aiogram import Dispatcher
 
 from app.bot.callbacks import router as callback_router
+from app.bot.handlers import download
 from app.bot.handlers import start, url
 
 
@@ -11,4 +12,5 @@ def create_dispatcher() -> Dispatcher:
     dp.include_router(start.router)
     dp.include_router(url.router)
     dp.include_router(callback_router)
+    dp.include_router(download.router)
     return dp
